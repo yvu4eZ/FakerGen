d12fe54c3f818f469abbaadf05865e7b65442578
Moe Abbas <yvu4eZ@protonmail.ch>
2022-11-06 21:06:50 +0100

    Changes to be committed: 	modified:   FakerRecordGenerator.py

--- FakerRecordGenerator.py
+++ FakerRecordGenerator.py
@@ -5,24 +5,24 @@ from datetime import datetime
 
 Fake = Faker('en_CA')
 
-def input_data(x):
-    Df = Panda.DataFrame()
+def fake_data(x):
+    FakerDf = Panda.DataFrame()
     for _ in range(0, x):
-        Df.loc[_,'uuid']= Fake.unique.uuid4(cast_to = None)
-        Df.loc[_,'type']= Fake.random_element(elements = ('subscriber', 'customer'))
-        Df.loc[_,'country']= Fake.current_country_code()
-        Df.loc[_,'first_name']= Fake.first_name()
-        Df.loc[_,'last_name']= Fake.last_name()
-        Df.loc[_,'email']= Fake.unique.ascii_company_email()
-        Df.loc[_,'phone']= Fake.unique.phone_number()
-        Df.loc[_,'job']= Fake.job()
-        Df.loc[_,'company']= Fake.unique.company()
-        Df.loc[_,'last_login_ip']= Fake.unique.ipv4()
-        Df.loc[_,'last_login']= Fake.date_time_between(start_date = "-1y", end_date = "now")
-        Df.loc[_,'created']= Fake.date_time_between(start_date = "-5y", end_date = "-1y")
-        Df.loc[_,'modified']= Fake.date_time_between(start_date = "-3y", end_date = "-1y")
-    return Df
+        FakerDf.loc[_,'uuid']= Fake.unique.uuid4(cast_to = None)
+        FakerDf.loc[_,'type']= Fake.random_element(elements = ('subscriber', 'customer'))
+        FakerDf.loc[_,'country']= Fake.current_country_code()
+        FakerDf.loc[_,'first_name']= Fake.first_name()
+        FakerDf.loc[_,'last_name']= Fake.last_name()
+        FakerDf.loc[_,'email']= Fake.unique.ascii_company_email()
+        FakerDf.loc[_,'phone']= Fake.unique.phone_number()
+        FakerDf.loc[_,'job']= Fake.job()
+        FakerDf.loc[_,'company']= Fake.unique.company()
+        FakerDf.loc[_,'last_login_ip']= Fake.unique.ipv4()
+        FakerDf.loc[_,'last_login']= Fake.date_time_between(start_date = "-1y", end_date = "now")
+        FakerDf.loc[_,'created']= Fake.date_time_between(start_date = "-5y", end_date = "-1y")
+        FakerDf.loc[_,'modified']= Fake.date_time_between(start_date = "-3y", end_date = "-1y")
+    return FakerDf
 
-Df = input_data(10)
+FakerDf = fake_data(1000)
 
-Df.to_csv(os.getcwd() + "/output_" + datetime.now().strftime("%Y-%m-%dT%H_%M_%S.%fZ") + '.csv')
+FakerDf.to_csv(os.getcwd() + "/output_" + datetime.now().strftime("%Y-%m-%dT%H_%M_%S.%fZ") + '.csv')
